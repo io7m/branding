43eb43bd219b5d0196d43b7e0b75cd34d5d95f10
Mark Raynsford <code@io7m.com>
2020-09-27 18:29:15 +0000

    Add icms

---
+++ make-one.sh
@@ -0,0 +1,33 @@
+#!/bin/sh -ex
+
+if [ $# -ne 1 ]
+then
+  echo "usage: name" 1>&2
+  exit 1
+fi
+
+name="$1"
+shift
+
+PROJECT_URL=$(cat "src/${name}/url.txt")
+PROJECT_DESCRIPTION=$(cat "src/${name}/description.txt")
+
+mkdir -p "out/${name}"
+rsync -av "src/${name}/" "out/${name}/"
+saxon \
+-xsl:src/social.xsl \
+-s:src/social.svg \
+projectName="${name}" \
+projectDescription="${PROJECT_DESCRIPTION}" \
+projectURL="${PROJECT_URL}" > "out/${name}/out.svg"
+
+pushd "out/${name}"
+inkscape \
+--export-type=png \
+--export-width=1280 \
+--export-height=640 \
+--export-filename=out.png \
+out.svg
+convert out.png out.jpg
+popd
+mv "out/${name}/out.jpg" "out/${name}.jpg"
--- make.sh
+++ make.sh
@@ -5,26 +5,5 @@ mkdir -p out
 
 for name in $(cat data.txt)
 do
-  PROJECT_URL=$(cat "src/${name}/url.txt")
-  PROJECT_DESCRIPTION=$(cat "src/${name}/description.txt")
-
-  mkdir -p "out/${name}"
-  rsync -av "src/${name}/" "out/${name}/"
-  saxon \
-  -xsl:src/social.xsl \
-  -s:src/social.svg \
-  projectName="${name}" \
-  projectDescription="${PROJECT_DESCRIPTION}" \
-  projectURL="${PROJECT_URL}" > "out/${name}/out.svg"
-
-  pushd "out/${name}"
-  inkscape \
-  --export-type=png \
-  --export-width=1280 \
-  --export-height=640 \
-  --export-filename=out.png \
-  out.svg
-  convert out.png out.jpg
-  popd
-  mv "out/${name}/out.jpg" "out/${name}.jpg"
+  ./make-one.sh "${name}"
 done
