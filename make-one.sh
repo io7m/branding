7d38e8162bb774edf0db191a6387224504b6d1a4
Mark Raynsford <code@io7m.com>
2021-06-26 20:25:27 +0000

    Add book cover generation

--- make-one.sh
+++ make-one.sh
@@ -23,7 +23,11 @@ projectURL="${PROJECT_URL}" > "out/${name}/out.svg"
 
 if [ -f "src/${name}/cover.png" ]
 then
-  convert "src/${name}/cover.png" "out/${name}/cover.jpg"
+  saxon \
+  -xsl:src/book_cover.xsl \
+  -s:src/book_cover.svg \
+  projectName="${name}" \
+  projectDescription="User Manual" > "out/${name}/book_cover.svg"
 fi
 
 pushd "out/${name}"
@@ -34,5 +38,22 @@ inkscape \
 --export-filename=out.png \
 out.svg
 convert out.png out.jpg
+
+if [ -f "book_cover.svg" ]
+then
+  inkscape \
+  --export-type=png \
+  --export-width=600 \
+  --export-height=800 \
+  --export-filename=book_cover.png \
+  book_cover.svg
+  convert book_cover.png book_cover.jpg
+fi
 popd
+
 mv "out/${name}/out.jpg" "out/${name}.jpg"
+
+if [ -f "src/${name}/cover.png" ]
+then
+  mv "out/${name}/book_cover.jpg" "out/${name}_book_cover.jpg"
+fi
